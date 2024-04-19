b00218089591246d9b5366d6db309ebfc744daa8
nour <nour.edaher.jouini@gmail.com>
2024-04-19 15:59:56 +0100

    adding tailwindcss

---
+++ coco-dashboard/tailwind.config.js
@@ -0,0 +1,13 @@
+const colors = require("./src/ui/theme/colors");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors,
+  },
+  plugins: [],
+};
